98b5f572ec69983412392bc49cfc6d32bb122209
DrunkUnicorn <fr.audrain@gmail.com>
2018-09-07 22:17:56 +0200

    Added file test_bissextile.py which determines whether or not a year is a leap year

---
+++ test_bissextile.py
@@ -0,0 +1,49 @@
+play = True
+
+while play:
+
+    
+
+    bissextile = False # le choix a été fait de fonctionner sur un objet booléen,
+                       # et non de se contenter d'afficher une réponse dans la
+                       # structure conditionnelle
+    valid_year = False
+
+
+    
+    
+    while valid_year != True:
+        year = input("Entrez une année : ")
+        try:
+            year = int(year)
+            valid_year = True
+        except:
+            print("L'année saisie est invalide")
+            
+        
+        
+
+    if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
+        bissextile = True
+    else:
+        bissextile = False
+
+        
+
+        
+
+    if bissextile:
+        print("L'année", year, "est bissextile")
+    else:
+        print("L'année", year, "n'est pas bissextile")
+
+        
+
+
+    choice = input("Tapez \'q\' pour quitter le programme, ou n'importe quelle \
+touche pour continuer, puis Entrée : ")
+
+    if choice.lower() == "q":
+        play = False
+    else:
+        play = True
