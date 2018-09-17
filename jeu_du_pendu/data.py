119c554a85bce2a30eeb4b9c0fd8407524a6ba98
DrunkUnicorn <fr.audrain@gmail.com>
2018-09-17 09:28:30 +0200

    Initialized the new dir "jeu_du_pendu" with the data file used by the game

---
+++ jeu_du_pendu/data.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*
+
+
+
+words_list = ["armoire",
+    "boucle",
+    "buisson",
+    "bureau",
+    "chaise",
+    "carton",
+    "couteau",
+    "fichier",
+    "garage",
+    "glace",
+    "journal",
+    "kiwi",
+    "lampe",
+    "liste",
+    "montagne",
+    "remise",
+    "sandale",
+    "taxi",
+    "vampire",
+    "volant",
+    ]
+
+
+remaining_strokes = 8
+
+
+score_file_name = "scores"
+
